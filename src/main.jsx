10d02371de6160b2653db207e08b226148f97e3f
Impossibilityy <111204168+Impossibilityy@users.noreply.github.com>
2026-02-08 20:05:43 -0500

    Render fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import StorflexAssistant from './ChatWidget'
+import StorflexAssistant from './ChatWidget.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
